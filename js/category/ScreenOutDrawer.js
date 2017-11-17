c6af528a15ffdf6afe0672eea3209d287b26ca98
歌神 <lee1059232202@163.com>
2017-11-17 15:34:49 +0800

    完成筛选界面

--- js/category/ScreenOutDrawer.js
+++ js/category/ScreenOutDrawer.js
@@ -1,31 +1,55 @@
 import React, {Component} from 'react'
-import {ScrollView, Text} from 'react-native'
+import {StyleSheet,ScrollView, View, Text, TouchableOpacity} from 'react-native'
 import {DrawerNavigator} from 'react-navigation';
 import CategoryDetailPage from "./CategoryDetailPage";
 import ScreenOutItem from "./ScreenOutItem";
+import {height} from "../common/Constants";
 
 
-/*const DrawerContent = (props) => (
-    <ScrollView style={{paddingLeft: 15, paddingRight: 15,}}>
-        <ScreenOutItem title='标题' list={data}/>
-        <ScreenOutItem title='我是'/>
-    </ScrollView>
-);*/
-
-
-class DrawerContent extends Component{
-    state={
-        data:[{key: '你好'}, {key: '他好'}, {key: '我好'}, {key: '大多'}, {key: '哈哈'}, {key: '呵呵哈'}, {key: '哈想'}],
+class DrawerContent extends Component {
+    state = {
+        types: ['全部', '促销优惠', '特卖', '自营', '众筹'],
+        brands: ['素士', '狮王', '高露洁', '佳洁士', '博朗', '黑人'],
+        categories: ['牙线', '牙刷', '牙线棒', '替换刷头', '牙签', '其他'],
+        materials: ['细毛', '纳米', '中毛', '碳丝', '硬毛', '其他'],
     }
-    render(){
+
+    render() {
         return (
-            <ScrollView style={{paddingLeft: 15, paddingRight: 15,}}>
-                <ScreenOutItem title='标题' list={this.state.data}/>
-                <ScreenOutItem title='我是' list={this.state.data}/>
-            </ScrollView>
+            <View>
+                <ScrollView style={{paddingLeft: 15, paddingRight: 15,height:height-44}}>
+                    <ScreenOutItem title='类型' list={this.state.types}/>
+                    <ScreenOutItem title='品牌' list={this.state.brands}/>
+                    <ScreenOutItem title='分类' list={this.state.categories}/>
+                    <ScreenOutItem title='材料' list={this.state.materials}/>
+                </ScrollView>
+                <View style={{flex: 1, flexDirection: 'row',height:44}}>
+                    <TouchableOpacity activeOpacity={0.7} style={[styles.button,{borderWidth:0.5,borderColor:'#979797'}]}>
+                        <Text style={{color:'#666',fontSize:17}}>重置</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity activeOpacity={0.7} onPress={this._onPress} style={[styles.button,{backgroundColor:'#9a7d56'}]}>
+                        <Text style={{color:'white',fontSize:17}}>确定</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+
         )
     }
+    _onPress=()=>{
+        this.props.navigation.navigate("DrawerClose");
+    }
+
 }
+
+const styles=StyleSheet.create({
+    button:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center',
+        height:44,
+        fontSize:17,
+    }
+});
 
 export const ScreenOutDrawer = DrawerNavigator(
     {
@@ -37,7 +61,7 @@ export const ScreenOutDrawer = DrawerNavigator(
         initialRouteName: 'CategoryDetail',
         drawerWidth: 320,
         drawerPosition: 'right',
-        contentComponent: (props)=><DrawerContent/>,
+        contentComponent: props => <DrawerContent {...props}/>,
         drawerBackgroundColor: 'white'
     }
 );
--- js/category/ScreenOutItem.js
+++ js/category/ScreenOutItem.js
@@ -1,19 +1,18 @@
 import React, {Component} from 'react'
 import {StyleSheet, Text, View, Image, FlatList, Animated, TouchableOpacity, Platform, StatusBar} from 'react-native';
 import PropTypes from 'prop-types'
-import {height} from "../common/Constants";
 
 const ICON_ARROW_DOWN = require('../../res/imgs/icon_arrow_down.png');
 export default class ScreenOutItem extends Component {
-    static defaultProps={
-        defaultShowSize:3,
-        list:[],
+    static defaultProps = {
+        showSize: 3,
+        list: [],
     }
 
     static propTypes = {
         title: PropTypes.string.isRequired,
-        list: PropTypes.array,
-        defaultShowSize:PropTypes.number,
+        list: PropTypes.array.isRequired,
+        showSize: PropTypes.number,
     }
 
     state = {
@@ -21,38 +20,37 @@ export default class ScreenOutItem extends Component {
     }
 
     render() {
-        let dataSource=[];
-        let dataBak=[];
-        this.props.list.map((item,i)=>{
-            item.index=i;
-            dataBak.push(item)
+        let dataSource = [];
+        let dataBak = [];
+        this.props.list.map((item, i) => {
+            item.index = i;
+            dataBak.push({
+                title: item,
+                index: i,
+            })
         });
         if (this.state.expanded) {
-            dataSource=dataBak;
-        }else {
-            if (this.props.defaultShowSize<this.props.list.length){
-                dataSource=dataBak.slice(0,this.props.defaultShowSize)
+            dataSource = dataBak;
+        } else {
+            if (this.props.showSize < this.props.list.length) {
+                dataSource = dataBak.slice(0, this.props.showSize)
             }
         }
-
-
 
         return (
             <View style={styles.container}>
                 <View style={{flexDirection: 'row', height: 44, alignItems: 'center',}}>
-                    <Text style={{color: "#666", fontSize: 14}}>{this.props.title}</Text>
-                    <View style={{flex: 1}}>
-                        <TouchableOpacity style={{justifyContent: 'flex-end',flexDirection: 'row',alignItems:'center'}} onPress={this._toggle}>
-                            <Text>全部</Text>
-                            <Image source={ICON_ARROW_DOWN}
-                                   style={{width: 10, height: 7,marginLeft:5}}/>
-                        </TouchableOpacity>
-                    </View>
+                    <Text style={{color: "#666", fontSize: 14,}}>{this.props.title}</Text>
+                    <View style={{flex: 1}}/>
+                    <TouchableOpacity style={styles.expandedItem} onPress={this._toggle}>
+                        <Text style={{fontSize:12,color:"#a0a0a0"}}>全部</Text>
+                        <Image source={ICON_ARROW_DOWN}
+                               style={[styles.arrowImage, {transform: [{rotateX: this.state.expanded ? '0deg' : '180deg'}]}]}/>
+                    </TouchableOpacity>
                 </View>
                 <FlatList
                     data={dataSource}
                     ItemSeparatorComponent={this._separator}
-                    columnWrapperStyle={{}}
                     renderItem={this._renderItem}
                     numColumns={3}
                 />
@@ -60,33 +58,21 @@ export default class ScreenOutItem extends Component {
         );
     }
 
-    _renderItem({item}){
+    _renderItem({item}) {
         return (
-            <View style={{width: 90, height: 30,justifyContent:'center',backgroundColor:'#fafafa',alignItems:'center',marginRight:item.index%3===2?0:10,borderRadius:4,borderWidth:1, borderColor:'#e6e6e6'}}>
-                <Text style={{ color: "#666",textAlign:'center'}}>{item.key}</Text>
-            </View>
+            <TouchableOpacity style={[styles.item, {marginRight: item.index % 3 === 2 ? 0 : 10,}]}>
+                <Text style={{color: "#666", textAlign: 'center'}}>{item.title}</Text>
+            </TouchableOpacity>
         );
     }
 
     _separator = () => {
-        return <View style={styles.separator} />
+        return <View style={styles.separator}/>
     }
 
-    _toggle=()=>{
+    _toggle = () => {
         this.setState({
-            expanded:!this.state.expanded,
-        });
-    }
-
-    _setMaxHeight=(event)=>{
-        this.setState({
-            maxHeight: event.nativeEvent.layout.height
-        });
-    }
-
-    _setMinHeight=(event)=>{
-        this.setState({
-            minHeight: event.nativeEvent.layout.height
+            expanded: !this.state.expanded,
         });
     }
 }
@@ -95,7 +81,29 @@ const styles = StyleSheet.create({
     container: {
         paddingTop: Platform.OS === 'ios' ? 20 : StatusBar.currentHeight,
     },
-    separator:{
-        height:10,
+    separator: {
+        height: 10,
+    },
+    item: {
+        width: 90,
+        height: 30,
+        justifyContent: 'center',
+        backgroundColor: '#fafafa',
+        alignItems: 'center',
+        borderRadius: 4,
+        borderWidth: 1,
+        borderColor: '#e6e6e6'
+    },
+    arrowImage: {
+        width: 10,
+        height: 7,
+        marginLeft: 5
+    },
+    expandedItem:{
+        justifyContent: 'flex-end',
+        paddingTop: 10,
+        paddingBottom:10,
+        flexDirection: 'row',
+        alignItems: 'center'
     }
 });
